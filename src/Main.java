b4bf7b9c28ae6cbc900265d0bc47192dc12c8c68
Екатерина Жирова <ketibez@gmail.com>
2022-05-26 20:54:12 +0300

    Commit mile

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int miles;
+        int ticketPrice = 8745;
+        int oneMile = 20;
+        miles = ticketPrice / oneMile;
+
+        System.out.println("Начисленные мили: " + miles);
+
+    }
+
+}
